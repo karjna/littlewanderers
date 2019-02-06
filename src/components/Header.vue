014df022c4d21396055536a86b3d7326f1d5cfbb
Karina Carmona <kgrjng@gmail.com>
2019-02-05 21:05:50 -0500

    header template with styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,65 @@
 <template>
-  <header class="header">
-
+  <header class="header" v-bind:class="[isHome ? 'home' : 'interior']">
+    <NavModal/>
+    <router-link to="/" class = "header__logo" v-if="!isHome">
+      <img src="../assets/img/interior.png" alt="Little Wanderers logo">
+    </router-link>
+    <div class = "float-right">
+      <router-link to="donate" class="header__link">Donate</router-link>
+      <router-link to="adopt" class="header__btn btn">Adopt</router-link>
+    </div>
   </header>
 </template>
 
 <script>
+import NavModal from './NavModal.vue';
+
 export default {
-  name: 'Header'
+  name: 'Header',
+  computed: {
+    isHome: function () {
+        return this.$route.name == 'home'
+    }
+  },
+  components: {
+    NavModal
+  }
 }
 </script>
 
-<style scoped>
-.header {
-  padding: 40px 0;
-  text-align: center;
-  background-color: black;
-}
+<style lang="scss">
+  .header{
+    padding: 20px 40px;
+    position: relative;
+    z-index: 2;
+  }
 
-.logo {
-  width: 140px;
-}
+  .header.home{
+    position: absolute;
+    width: 100%;
+  }
+
+  .header.interior{
+    border-bottom: 1px solid $lightgray;
+    padding: 40px 40px 10px;
+    margin-bottom: 60px;
+  }
+
+  .header__link{
+    @include courier;
+    margin-right: 25px;
+  }
+
+  .header__logo{
+    display: inline-block;
+    margin: 0 auto;
+    width:250px;
+    position: absolute;
+    top: 0px;
+    left: 0;
+    right: 0;
+    img{
+      width: 100%;
+    }
+  }
 </style>
