e0598816873efdd49894a37c5c304325b5c14256
Karina Carmona <kgrjng@gmail.com>
2019-02-08 10:45:38 -0500

    Donate page

---
+++ src/views/Donate.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="container">
+    <h1>Donate</h1>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Donate',
+  data () {
+    return {
+      cats: null
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
