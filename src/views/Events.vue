92a7212c418cdfc38cb4fd4fc7f62315ee8760d2
Karina Carmona <kgrjng@gmail.com>
2019-02-08 10:45:44 -0500

    Events page

---
+++ src/views/Events.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="container">
+    <h1>Adoption Events</h1>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Events',
+  data () {
+    return {
+      cats: null
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
