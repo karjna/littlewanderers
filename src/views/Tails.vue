f33a6e91225f8e35c389b5a60747f34632baa4e4
Karina Carmona <kgrjng@gmail.com>
2019-02-08 10:45:57 -0500

    happy tails

---
+++ src/views/Tails.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="container">
+    <h1>Happy Tails</h1>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Tails',
+  data () {
+    return {
+      cats: null
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
