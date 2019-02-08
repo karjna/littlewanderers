d12ab0ad88d4fe47dd712c5f8f54970a427570b8
Karina Carmona <kgrjng@gmail.com>
2019-02-08 10:46:08 -0500

    volunteer page

---
+++ src/views/Volunteer.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="container">
+    <h1>Volunteer</h1>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Volunteer',
+  data () {
+    return {
+      cats: null
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
