bb0cb4ff0db30a638de57f1a05ae9d327c460dba
Karina Carmona <kgrjng@gmail.com>
2019-02-08 10:45:48 -0500

    Foster template

---
+++ src/views/Foster.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="container">
+    <h1>Foster</h1>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Foster',
+  data () {
+    return {
+      cats: null
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
