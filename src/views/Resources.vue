778c65c65688c76708009908679640817c9e2467
Karina Carmona <kgrjng@gmail.com>
2019-02-08 10:45:53 -0500

    resources page

---
+++ src/views/Resources.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="container">
+    <h1>Rescue Resources</h1>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Resources',
+  data () {
+    return {
+      cats: null
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
