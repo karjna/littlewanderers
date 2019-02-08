94f20efce249a3d50bb1ae2a972230260e5f0fae
Karina Carmona <kgrjng@gmail.com>
2019-02-08 10:44:37 -0500

    extra spce

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,14 @@
 import Footer from './components/Footer.vue'
 import Header from './components/Header.vue'
 
+
 export default {
   name: 'App',
   components: {
     Footer,
     Header
   }
+
 }
 </script>
 
