3650cade94c0aa3f212f812a2eb3a8878fd9abe4
Karina Carmona <kgrjng@gmail.com>
2019-02-08 10:44:49 -0500

    Fix all footer links to router links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,21 +2,21 @@
   <footer class="footer">
     <div class = 'container'>
       <div class = "footer__links">
-        <router-link to="about" class="footer__link">About</router-link>
-        <router-link to="cats" class="footer__link">Adopt</router-link>
-        <router-link to="foster" class="footer__link">Foster</router-link>
-        <router-link to="tails" class="footer__link">Happy Tails</router-link>
-        <router-link to="volunteer" class="footer__link">Volunteer</router-link>
-        <router-link to="resources" class="footer__link">Rescue Resources</router-link>
-        <router-link to="donate" class="footer__link">Donate</router-link>
-        <router-link to="events" class="footer__link">Adoption Events</router-link>
-        <router-link to="shop" class="footer__link">Shop</router-link>
-        <router-link to="contact" class="footer__link">Contact</router-link>
+        <router-link :to="{ name: 'about'}" class="footer__link">About</router-link>
+        <router-link :to="{ name: 'cats'}" class="footer__link">Adopt</router-link>
+        <router-link :to="{ name: 'foster'}"class="footer__link">Foster</router-link>
+        <router-link :to="{ name: 'tails'}"class="footer__link">Happy Tails</router-link>
+        <router-link :to="{ name: 'volunteer'}" class="footer__link">Volunteer</router-link>
+        <router-link :to="{ name: 'resources'}" class="footer__link">Rescue Resources</router-link>
+        <router-link :to="{ name: 'donate'}" class="footer__link">Donate</router-link>
+        <router-link :to="{ name: 'events'}" class="footer__link">Adoption Events</router-link>
+        <router-link :to="{ name: 'shop'}" class="footer__link">Shop</router-link>
+        <router-link :to="{ name: 'contact'}" class="footer__link">Contact</router-link>
       </div>
       <div class = "float-right social-footer">
         <span class = "social-link__text">Follow us on social media: </span>
         <a href = "" target = "_blank" class = "social-link"><i class="fab fa-facebook-f"></i></a>
-        <a href = "" target = "_blank" class = "social-link"><i class = "fab fa-instagram"></i></a>
+        <a href = "https://www.instagram.com/littlewanderersnyc/?hl=en" target = "_blank" class = "social-link"><i class = "fab fa-instagram"></i></a>
       </div>
     </div>
 
@@ -31,22 +31,23 @@
     <div class="modal fade" id="navModal" tabindex="-1" role="dialog" aria-labelledby="navModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
-          <div class="modal-header">
+          <div class = "row">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
+
             <router-link to="about" class="nav__link">About</router-link>
-            <router-link to="cats" class="nav__link">Adopt</router-link>
-            <router-link to="foster" class="nav__link">Foster</router-link>
-            <router-link to="tails" class="nav__link">Happy Tails</router-link>
-            <router-link to="volunteer" class="nav__link">Volunteer</router-link>
-            <router-link to="resources" class="nav__link">Rescue Resources</router-link>
-            <router-link to="donate" class="nav__link">Donate</router-link>
-            <router-link to="events" class="nav__link">Adoption Events</router-link>
-            <router-link to="shop" class="nav__link">Shop</router-link>
-            <router-link to="contact" class="nav__link">Contact</router-link>
+            <router-link :to="{ name: 'cats'}" class="nav__link">Adopt</router-link>
+            <router-link :to="{ name: 'foster'}" class="nav__link">Foster</router-link>
+            <router-link :to="{ name: 'tails'}" class="nav__link">Happy Tails</router-link>
+            <router-link :to="{ name: 'volunteer'}" class="nav__link">Volunteer</router-link>
+            <router-link :to="{ name: 'resources'}" class="nav__link">Rescue Resources</router-link>
+            <router-link :to="{ name: 'donate'}" class="nav__link">Donate</router-link>
+            <router-link :to="{ name: 'events'}" class="nav__link">Adoption Events</router-link>
+            <router-link :to="{ name: 'shop'}" class="nav__link">Shop</router-link>
+            <router-link :to="{ name: 'contact'}" class="nav__link">Contact</router-link>
           </div>
 
         </div>
@@ -56,8 +57,16 @@
 </template>
 
 <script>
+import JQuery from 'jquery'
+let $ = JQuery
+
   export default {
-    name: 'Footer'
+    name: 'Footer',
+    watch: {
+      '$route' () {
+        $('#navModal').modal('hide');
+      }
+    }
   }
 </script>
 
@@ -119,9 +128,10 @@
   background: transparent;
   border:0px;
   .close{
-    color: white;
+    color: $yellow;
     opacity: 1;
-    font-size: 58px;
+    font-size: 80px;
+    margin: 15px 10px 0;
   }
 }
 
@@ -135,15 +145,20 @@
 
 .modal-body{
   column-count: 2;
+  max-width: 900px;
+  margin: 0 auto;
 }
 
 .nav__link{
   display: block;
-  @include courier-bold;
-  font-size: 48px;
-    margin: 14px;
+  font-size: 35px;
+  margin: 14px;
+  color: white;
+  font-weight: 900;
 }
 .modal-backdrop.show {
     opacity: .95;
+    background-image: url("../assets/img/black-bg.jpg");
+        border: 14px solid $yellow;
 }
 </style>
