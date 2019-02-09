296e86db8962df08e8900f77e9f8711f7f480cc9
Karina Carmona <kgrjng@gmail.com>
2019-02-08 19:59:20 -0500

    Foster vue

--- src/views/Foster.vue
+++ src/views/Foster.vue
@@ -2,6 +2,18 @@
 
   <div class="container">
     <h1>Foster</h1>
+
+
+<p class = "intro">Becoming a foster parent is the best way to help cats in need! Foster parents allow organizations to rescue more cats! As a foster, you will provide a cat/cats a loving home where they can flourish until they find a FURever home!</p>
+
+<p class ="intro">Little Wanderers NYC does not have a shelter location, and therefore we rely solely on foster parents to care for our cats! We are ALWAYS in need of more fosters.</p>
+
+<p class="intro">If you are interested in becoming a Little Wanderers foster parent, please fill out our online application below.</p>
+
+<div class = "text-center">
+  <a href = "https://docs.google.com/forms/d/e/1FAIpQLSfglgaToBPXtODnhjP-nQo-XVU0tcFOZQ1IgaMbkoyofOyr5Q/viewform" target="_blank" class = "btn">Foster Applicaiton</a>
+</div>
+<p class="mt-4 text-center">To contact our foster manager, Olivia, please email <a href = "mailto:fosteringlwnyc@gmail.com">fosteringlwnyc@gmail.com</a>. </p>
   </div>
 
 </template>
